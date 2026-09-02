3be47ede31198ac4a90ea2d48f294549e17cc3d2
Richard Canoa Cachi <rcanoa@ucb.edu.bo>
2026-09-02 17:29:15 -0400

    Iniciar el proyecto Json Demo

---
+++ Json-Demo/Controllers/DemoController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+
+namespace Json_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        public DemoController()
+        {
+
+        }
+
+        [HttpGet("generica")]
+        public IActionResult ListaGenerica()
+        {
+            var personas = new List<Persona>()
+            {
+                new Persona
+                { 
+                    Id = 1, 
+                    Nombre = "Juan", 
+                    Edad = 18 
+                },
+                new Persona
+                {
+                    Id = 2,
+                    Nombre = "Maria",
+                    Edad = 17
+                }
+            };
+
+            return Ok(personas);
+
+        }
+
+        [HttpGet("diccionario")]
+        public IActionResult Diccionarios()
+        {
+            var diccionario = new Dictionary<string, string>
+            {
+                { "clave1", "valor1" },
+                { "clave2", "valor2" },
+                { "clave3", "valor3" },
+                { "clave4", "valor4" },
+                { "clave5", "valor5" }
+            };
+
+            return Ok(diccionario);
+        }
+
+        [HttpGet("dinamico")]
+        public IActionResult ObjetoDinamico()
+        {
+            List<object> lista = new List<object>();
+            dynamic objDinamico = new ExpandoObject();
+            objDinamico.Id = 1;
+            objDinamico.Nombre = "Juan";
+            objDinamico.Edad = 18;
+            lista.Add(objDinamico);
+
+            objDinamico = new ExpandoObject();
+            objDinamico.Id = 1;
+            objDinamico.Nombre = "Juan";
+            objDinamico.Edad = 18;
+            lista.Add(objDinamico);
+
+            objDinamico = new ExpandoObject();
+            objDinamico.Id = 1;
+            objDinamico.Nombre = "Juan";
+            objDinamico.Edad = 18;
+            lista.Add(objDinamico);
+
+            return Ok(objDinamico);
+        }
+    }
+}
---
+++ Json-Demo/Persona.cs
@@ -0,0 +1,9 @@
+﻿namespace Json_Demo
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
